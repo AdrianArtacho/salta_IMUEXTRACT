d8acb53122ccca047f89b39fd02b968c5c862e34
Adrian Artacho <adrian.artacho@gmail.com>
2024-02-07 15:00:01 +0100

    …increment.

--- IMUEXTRACT.py
+++ IMUEXTRACT.py
@@ -5,29 +5,29 @@ import time_signature as time_signature
 import time_midi as time_midi
 import time_stamps as time_stamps
 
-def main(midi_file = 'INPUT/Anna-RAW.mid',
+def main(midi_file = 'Anna-RAW.mid',
          input_path='INPUT/',
          output_path='OUTPUT/',
          streams_path='stream_data/',
          time_length_str = '',
          time_signature_str = '',
          verbose=False):
-   
+    
     # Specify the path to your MIDI file
     midi_file_path = input_path+midi_file
+    if verbose:
+        print("midi_file_path:", midi_file_path)
 
-
-    max_datapoint = 454831
+    max_datapoint = 454831      # ???
     dps_rounded = time_midi.main(max_datapoint,
                                 time_length_str = time_length_str, #'39:32.032', 
                                 max_bars = 1186,
                                 time_bpm = 120)
     
-    print("dps_rounded:", dps_rounded)
+    if verbose:
+        print("dps_rounded:", dps_rounded, "(It should be about 192)")
     
-
     # exit()
-
     # Initialize a dictionary to store CC data for different streams
     cc_data = {}
 
@@ -36,6 +36,8 @@ def main(midi_file = 'INPUT/Anna-RAW.mid',
 
     # Iterate through the MIDI file and extract relevant CC information
     for track in mid.tracks:
+        # if verbose:
+        #     print("track:", track)
         time_elapsed = 0  # Initialize time elapsed in ticks
         
         for msg in track:
@@ -50,20 +52,32 @@ def main(midi_file = 'INPUT/Anna-RAW.mid',
                         'Control_Value': []
                     }
                 
+                # if control_number == 18:
+                #     print("control_number:", control_number,"time_elapsed:", time_elapsed)
+
                 # Append data to the respective CC stream
                 cc_data[control_number]['Timestamp'].append(time_elapsed)
                 cc_data[control_number]['Control_Value'].append(msg.value)
 
+                # if verbose:
+                #     print(cc_data[control_number])
+
+    # exit()
     # Create separate DataFrames for each CC stream
     for control_number, data in cc_data.items():
         cc_df = pd.DataFrame(data)
+
+        # Group by 'Timestamp' and keep the last entry of each group
+        cc_df = cc_df.groupby('Timestamp').last().reset_index()
+
         csv_filename = f'{streams_path}CC{control_number}_data.csv'
         cc_df.to_csv(csv_filename, index=False)
 
         print(f'Saved CC{control_number} data as {streams_path+csv_filename}')
 
-    print("cc_data:")
-    print(cc_data.items())
+    # print("cc_data:")
+    # print(cc_data.items())
+        
     # Iterate through the MIDI file and print its contents
     # for i, track in enumerate(mid.tracks):
     #     print(f"Track {i}:")
--- README.md
+++ README.md
@@ -6,4 +6,26 @@ This script(s) process the raw input from different IMU devices.
 
 ## SOMI (IMU data as MIDI)
 
-- How should I proceed with this
+#### Usage:
+
+Activate venv
+
+```python
+source .venv/bin/activate
+```
+
+Run main script:
+
+```python
+python IMUEXTRACT.py
+```
+
+
+
+---
+
+### Open questions
+
+- How do I find out the `max_datapoint` in general?
+
+- Is it a problem that I don't have data for ALL the timestamp values? should I resample it to the same sampling as MPIPE or AUDIO
